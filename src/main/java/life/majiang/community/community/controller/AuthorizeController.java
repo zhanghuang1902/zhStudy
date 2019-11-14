06f1386311d02c34455b987f0d7d8e8c502be236
zhanghuang1902 <970839007@qq.com>
2019-11-14 23:40:23 +0800

    提交文件

--- src/main/java/life/majiang/community/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/community/controller/AuthorizeController.java
@@ -1,15 +1,13 @@
 package life.majiang.community.community.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+public class AuthorizeController {
+    @GetMapping("callback")
+    public String callback(@RequestParam(name = "code") String code ,@RequestParam("name=state") String state){
+        return  "index";
     }
 }
