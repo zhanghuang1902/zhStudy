5d7bd26516d044fcedf8d58ce6ed7677e9f5c089
zhanghuang1902 <970839007@qq.com>
2019-11-17 09:41:10 +0800

    修改properties

--- src/main/java/life/majiang/community/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/community/controller/AuthorizeController.java
@@ -5,6 +5,7 @@ import life.majiang.community.community.dto.AccessToken;
 import life.majiang.community.community.provider.GitHubAccessToken;
 import life.majiang.community.community.provider.GitHubUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,26 +13,30 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class AuthorizeController {
     //8cd120291a25422f9c04b90c3cecaaa76fe9c184
-   @Autowired
-   private GitHubAccessToken gitHubAccessToken;
+    @Autowired
+    private GitHubAccessToken gitHubAccessToken;
 
-   @Autowired
-   private GitHubUser gitHubUser;
+    @Autowired
+    private GitHubUser gitHubUser;
+    @Value("${github.Client_id}")
+    private String Client_id;
+    @Value("${github.Client_secret}")
+    private String Client_secret;
+    @Value("${github.Redirect_uri}")
+    private String Redirect_uri;
 
     @GetMapping("/callback")
-    public String callback(@RequestParam(name = "code") String code ,@RequestParam(name="state") String state){
+    public String callback(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state) {
 
         AccessToken accessToken = new AccessToken();
-        accessToken.setClient_id("f2edf418df074bb1dc01");
-        accessToken.setClient_secret("9591b2caa4a91415fe95b68cb23e1713a60de07e");
+        accessToken.setClient_id(Client_id);
+        accessToken.setClient_secret(Client_secret);
         accessToken.setCode(code);
-        accessToken.setRedirect_uri("http://localhost:8887/callback");
+        accessToken.setRedirect_uri(Redirect_uri);
         accessToken.setState(state);
         String body = gitHubAccessToken.getAccessToken(accessToken);
         AccessGitHubUser user = gitHubUser.getUser(body);
         System.out.println(user.getName());
-        return  "index";
-
+        return "index";
     }
-
 }
